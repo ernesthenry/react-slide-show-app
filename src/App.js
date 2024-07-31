c4835710429636b2f6e9ab7be9c094fa7a8fa76c
ernesthenry <henry38ernest@gmail.com>
2024-07-31 17:28:23 +0300

    Add sample data

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ import 'h8k-components'
 import Slides from './components/Slides'
 const title = 'Slideshow App'
 
-function App({ slides }) {
+function App() {
+  // Sample slides data
+  const slides = [
+    { title: 'Slide 1', text: 'This is the text for slide 1.' },
+    { title: 'Slide 2', text: 'This is the text for slide 2.' },
+    { title: 'Slide 3', text: 'This is the text for slide 3.' },
+    { title: 'Slide 4', text: 'This is the text for slide 4.' },
+    { title: 'Slide 5', text: 'This is the text for slide 5.' },
+  ];
   return (
     <div>
       <h8k-navbar header={title}></h8k-navbar>
